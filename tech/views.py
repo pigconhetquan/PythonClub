82dde730169d594804c19f93e877fe33bc81e08b
Tuan Phan <tuantphan83@gmail.com>
2021-01-19 21:13:53 -0800

    Resources view

--- tech/views.py
+++ tech/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resources, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'tech/index.html')
+
+def resources(request):
+    resource_list=Resources.objects.all()
+    return render(request, 'tech/resources.html', {'resource_list': resource_list})
